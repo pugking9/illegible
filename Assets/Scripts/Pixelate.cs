3e0fd5ea6b0933c4b9488c5e3571e3e65a61a603
pugking9 <pugking9@gmail.com>
2015-12-14 19:03:01 -0500

    Rebuild, added everything, needs further tweaking

---
+++ Assets/Scripts/Pixelate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pixelate : MonoBehaviour {
+
+    public Color edgeColor;
+    public float numTiles = 8.0f;
+    public float threshhold = 1.0f;
+    public Camera renderTexCam;
+
+    [HideInInspector]
+    public Material blurMat;
+    public Shader pixelater;
+
+    RenderTexture TarTex;
+
+	// Use this for initialization
+	void Start () {
+        TarTex = new RenderTexture(Screen.currentResolution.width, Screen.currentResolution.height, 16);
+
+        blurMat = new Material(pixelater);
+
+        renderTexCam.targetTexture = TarTex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnPostRender ()
+    {
+        blurMat.SetTexture("_MainTex", TarTex);
+        blurMat.SetColor("_EdgeColor", edgeColor);
+        blurMat.SetFloat("_NumTiles", numTiles);
+        blurMat.SetFloat("_Threshhold", threshhold);
+        Graphics.Blit(TarTex, blurMat);
+    }
+}
---
+++ Assets/Scripts/RigifbodyWalker.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigifbodyWalker : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float gravity = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public bool canJump = true;
+    public float jumpHeight = 2.0f;
+    public Camera playerCam;
+    private bool grounded = false;
+
+
+
+    void Awake()
+    {
+        GetComponent<Rigidbody>().freezeRotation = true;
+        GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity *= speed;
+
+        Vector3 velocity = GetComponent<Rigidbody>().velocity;
+        Vector3 velocityChange = (targetVelocity - velocity);
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = 0;
+        GetComponent<Rigidbody>().AddForce(velocityChange, ForceMode.VelocityChange);
+
+        if (grounded)
+        {
+            if (canJump && Input.GetButton("Jump"))
+            {
+                GetComponent<Rigidbody>().velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            }
+        }
+
+        GetComponent<Rigidbody>().AddForce(new Vector3(0, -gravity * GetComponent<Rigidbody>().mass, 0));
+
+        grounded = false;
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    float CalculateJumpVerticalSpeed()
+    {
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
+    }
+}
